f7fa79d20f72098e6b653a01f6f20136ea2bebc6
lucca <davilucarios47@gmail.com>
2025-09-03 22:55:44 -0300

    header okk

---
+++ src/components/Header/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <header>
+            <h1>Davi Lucas Rios Da Cunha</h1>
+            <img src="/images/eu.jpeg" alt="eu.jpeg" />
+            <p>No Pain No Gain</p>
+        </header>
+    )
+}
