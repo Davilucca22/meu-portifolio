4e931ea5919ec6858a6b2745d4e6e38d69ec2247
lucca <davilucarios47@gmail.com>
2025-09-10 20:35:37 -0300

    teste3

--- src/components/Conhecimentos/Conhecimentos.js
+++ src/components/Conhecimentos/Conhecimentos.js
@@ -3,31 +3,31 @@ import './Conhecimentos.css'
 
 export default function Conhecimentos(){
     return(
-        <div id="conteinerCards">
-            <div className="card">
+        <div id="conteinercards">
+            <div className="cards">
                 <img src="./images/iconeHTML.png" alt="icone HTML"></img>
                 <h2>HTML</h2>
                 <p>HTML é a linguagem de marcação responsável pela estruturação do conteúdo na web. Ele define a hierarquia e o significado semântico dos elementos, como títulos, parágrafos, listas, links, imagens e formulários</p>
             </div>
-            <div className="card">
+            <div className="cards">
                 <img src="./images/iconeCSS.png" alt="icone CSS"></img>
                 <h2>CSS</h2>
-                <p>HTML é a linguagem de marcação responsável pela estruturação do conteúdo na web. Ele define a hierarquia e o significado semântico dos elementos, como títulos, parágrafos, listas, links, imagens e formulários</p>
+                <p>CSS é a linguagem utilizada para definir a apresentação visual de documentos HTML. Por meio dela, é possível aplicar estilos como cores, tipografia, espaçamentos, layouts responsivos e animações. Suporta diferentes paradigmas, como Flexbox e Grid, permitindo a criação de interfaces escaláveis e adaptáveis a múltiplos dispositivos.</p>
             </div>
-            <div className="card">
+            <div className="cards">
                 <img src="./images/iconeJS.png" alt="icone HTML"></img>
                 <h2>JavaScript</h2>
-                <p>HTML é a linguagem de marcação responsável pela estruturação do conteúdo na web. Ele define a hierarquia e o significado semântico dos elementos, como títulos, parágrafos, listas, links, imagens e formulários</p>
+                <p>JavaScript é uma linguagem de programação interpretada, orientada a eventos e de tipagem dinâmica, utilizada principalmente no desenvolvimento web.Hoje é amplamente utilizado tanto no front-end quanto no back-end, graças ao Node.js.</p>
             </div>
-            <div className="card">
+            <div className="cards">
                 <img src="./images/iconeNODE.png" alt="icone HTML"></img>
                 <h2>Node.js</h2>
-                <p>HTML é a linguagem de marcação responsável pela estruturação do conteúdo na web. Ele define a hierarquia e o significado semântico dos elementos, como títulos, parágrafos, listas, links, imagens e formulários</p>
+                <p>Node.js é um ambiente de execução JavaScript baseado no motor V8 do Google Chrome. Ele permite executar código JavaScript no lado do servidor, oferecendo recursos para criação de APIs, manipulação de arquivos, gerenciamento de pacotes (NPM) e integração com bancos de dados. Sua arquitetura orientada a eventos e não bloqueante o torna altamente eficiente para aplicações escaláveis.</p>
             </div>
-            <div className="card">
+            <div className="cards">
                 <img src="./images/iconeREACT.png" alt="icone HTML"></img>
                 <h2>React</h2>
-                <p>HTML é a linguagem de marcação responsável pela estruturação do conteúdo na web. Ele define a hierarquia e o significado semântico dos elementos, como títulos, parágrafos, listas, links, imagens e formulários</p>
+                <p>React é uma biblioteca JavaScript de código aberto voltada para o desenvolvimento de interfaces de usuário. Baseado em componentes reutilizáveis, utiliza o conceito de Virtual DOM para otimizar a atualização da interface. React facilita a criação de aplicações de página única (SPAs) com gerenciamento de estado, escalabilidade e integração com bibliotecas auxiliares como Redux e React Router.</p>
             </div>
         </div>
     )
--- src/components/Projetos/projetos.js
+++ src/components/Projetos/projetos.js
@@ -10,7 +10,7 @@ export default function Projetos(){
         <h3 id="titulo">Projetos</h3>
             <div className="card">
                 <h2>Creatina Page</h2>
-                <img src="./images/paginaCreatina.PNG" alt="print do site"></img>
+                <img className="img" src="./images/paginaCreatina.PNG" alt="print do site"></img>
                 <p>um site desenvolvido para mostrar os beneficios e usos da Creatina</p>
                 <p>CSS + HTML</p>
                 <a href="https://github.com/Davilucca22/Creatine-Page" target="blank_" ><FaGithub /></a> 
@@ -19,7 +19,7 @@ export default function Projetos(){
 
             <div className="card">
                 <h2>Lista de Tarefas</h2>
-                <img src="./images/listaDeTarefas.png" alt="print do site"></img>
+                <img className="img" src="./images/listaDeTarefas.png" alt="print do site"></img>
                 <p>uma lista de Tarefas simples com dados persistentes e responsividade</p>
                 <p>CSS + HTML + Javascript</p>
                 <a href="https://github.com/Davilucca22/Lista-de-Tarefas" target="blank_" ><FaGithub /></a> 
@@ -28,7 +28,7 @@ export default function Projetos(){
 
             <div className="card">
                 <h2>Tensao X Corrente</h2>
-                <img src="./images/correnteXtensao.png" alt="print do site"></img>
+                <img className="img" src="./images/correnteXtensao.png" alt="print do site"></img>
                 <p>site desenvolvido para cliente para fins industriais, carga de bancos de baterias de chumbo</p>
                 <p>CSS + HTML + Javascript</p>
                 <a href="https://github.com/Davilucca22/Tensao-Corrente" target="blank_" ><FaGithub /></a> 
@@ -37,7 +37,7 @@ export default function Projetos(){
 
             <div className="card">
                 <h2>Site dia dos namorados</h2>
-                <img src="./images/diadosnamorados.png"  alt="print so site"></img>
+                <img className="img" src="./images/diadosnamorados.png"  alt="print so site"></img>
                 <p>um site desenvolvido para presentear quem voce ama em datas comemorativas</p>
                 <p>CSS + HTML + Javascript</p>
                 <a href="https://github.com/Davilucca22/site-dia-dos-namorados" target="blank_" ><FaGithub /></a> 
